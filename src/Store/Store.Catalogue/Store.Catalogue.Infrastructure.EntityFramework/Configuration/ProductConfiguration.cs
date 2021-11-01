3e1d5d58027d8451b964b971d87956620c1a90ab
Emanuel Skrenkovic <eskrenkovic@gmail.com>
2021-11-01 12:31:16 +0100

    added db set to database context

--- src/Store/Store.Catalogue/Store.Catalogue.Infrastructure.EntityFramework/Configuration/ProductConfiguration.cs
+++ src/Store/Store.Catalogue/Store.Catalogue.Infrastructure.EntityFramework/Configuration/ProductConfiguration.cs
@@ -8,7 +8,7 @@ namespace Store.Catalogue.Infrastructure.EntityFramework.Configuration
     {
         public void Configure(EntityTypeBuilder<ProductDisplayEntity> builder)
         {
-            builder.ToTable("product");
+            builder.ToTable("product_display");
             
             builder.HasKey(p => p.Id);
             builder.Property(p => p.Id).HasColumnName("id");
--- src/Store/Store.Catalogue/Store.Catalogue.Infrastructure.EntityFramework/StoreCatalogueDbContext.cs
+++ src/Store/Store.Catalogue/Store.Catalogue.Infrastructure.EntityFramework/StoreCatalogueDbContext.cs
@@ -1,14 +1,21 @@
 using Microsoft.EntityFrameworkCore;
+using Store.Catalogue.Infrastructure.EntityFramework.Entity;
 
 namespace Store.Catalogue.Infrastructure.EntityFramework
 {
     public class StoreCatalogueDbContext : DbContext
     {
+        public DbSet<ProductDisplayEntity> ProductDisplays { get; set; }
+        
         public StoreCatalogueDbContext(DbContextOptions<StoreCatalogueDbContext> options) : base(options) { }
         
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.HasDefaultSchema("public");
+            
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(StoreCatalogueDbContext).Assembly);
+            
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
