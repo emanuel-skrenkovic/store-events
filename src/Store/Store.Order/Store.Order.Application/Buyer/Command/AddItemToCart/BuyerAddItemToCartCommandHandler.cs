2db17b9ef6443f81e6f38262ab04ca74d46aeec9
Emanuel Skrenkovic <eskrenkovic@gmail.com>
2021-11-11 17:15:46 +0100

    implemented add cart command

--- src/Store/Store.Order/Store.Order.Application/Buyer/Command/AddItemToCart/BuyerAddItemToCartCommandHandler.cs
+++ src/Store/Store.Order/Store.Order.Application/Buyer/Command/AddItemToCart/BuyerAddItemToCartCommandHandler.cs
@@ -3,15 +3,46 @@ using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using Store.Core.Domain.Result;
+using Store.Order.Domain.Buyers;
 using Unit = Store.Core.Domain.Functional.Unit;
 
 namespace Store.Order.Application.Buyer.Command.AddItemToCart
 {
     public class BuyerAddItemToCartCommandHandler : IRequestHandler<BuyerAddItemToCartCommand, Result<Unit>>
     {
-        public Task<Result<Unit>> Handle(BuyerAddItemToCartCommand request, CancellationToken cancellationToken)
+        private readonly IBuyerRepository _buyerRepository;
+
+        public BuyerAddItemToCartCommandHandler(IBuyerRepository buyerRepository)
         {
-            throw new NotImplementedException();
+            _buyerRepository = buyerRepository ?? throw new ArgumentNullException(nameof(buyerRepository));
         }
-    }
+        
+        public async Task<Result<Unit>> Handle(BuyerAddItemToCartCommand request, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            Domain.Buyers.Buyer buyer = await _buyerRepository.GetBuyerAsync(request.CustomerNumber);
+            bool isNew = buyer == null;
+
+            if (isNew)
+            {
+                buyer = Domain.Buyers.Buyer.Create(request.CustomerNumber);
+            }
+            
+            // TODO: check if warning correct.
+            buyer.AddCartItem(request.Item);
+
+            // TODO: Should really consolidate these. This is stupid.
+            if (isNew)
+            {
+                await _buyerRepository.CreateBuyerAsync(buyer);
+            }
+            else
+            {
+                await _buyerRepository.SaveBuyerAsync(buyer);
+            }
+
+            return Unit.Value;
+        }
+   }
 }
