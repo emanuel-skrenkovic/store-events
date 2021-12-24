2b761014e24cc2248584f146cc6657cd12b9e667
Emanuel Skrenkovic <eskrenkovic@gmail.com>
2021-12-24 16:06:43 +0100

    updated postgres test fixture to use Docker container

--- src/Store/Store.Core/Store.Core.Tests.Infrastructure/PostgresFixture.cs
+++ src/Store/Store.Core/Store.Core.Tests.Infrastructure/PostgresFixture.cs
@@ -8,35 +8,84 @@ public class PostgresFixture<TContext> : IDisposable where TContext : DbContext
     private const string ConnectionString = 
         "User ID=postgres;Password=postgres;Server=localhost;Port=5432;Database=store-shopping;Integrated Security=true;Pooling=true;";
     
-    private readonly TContext _context;
+    #region DockerParameters
+
+    private const string ContainerName = "store.integration-tests.postgres";
+    private const string ImageName = "postgres:14-alpine";
+
+    private readonly List<string> _env = new()
+    {
+        "POSTGRES_USER=postgres",
+        "POSTGRES_PASSWORD=postgres"
+    };
+
+    private readonly Dictionary<string, string> _ports = new()
+    {
+        ["5432"] = "5432"
+    };
+    
+    #endregion
+    
+    private TContext _context;
+    private readonly DockerContainer _container;
 
     public PostgresFixture()
     {
-        DbContextOptionsBuilder<TContext> optionsBuilder = new();
-        optionsBuilder.UseNpgsql(ConnectionString);
+        _container = new(ContainerName, ImageName, _env, _ports);
+        _container.EnsureRunningAsync(CheckConnectionAsync)
+            .ConfigureAwait(false)
+            .GetAwaiter()
+            .GetResult();
         
-        _context = (TContext) Activator.CreateInstance(
-            typeof(TContext), 
-            optionsBuilder.Options);
+        EnsureMigratedAsync()
+            .ConfigureAwait(false)
+            .GetAwaiter()
+            .GetResult();
     }
 
     public async Task SeedAsync(Func<TContext, Task> seedAction)
     {
-        await EnsureDatabaseAsync();
+        await EnsureMigratedAsync();
         await seedAction(_context);
     }
 
     public async Task CleanAsync()
     {
         await _context.Database.EnsureDeletedAsync();
-        await EnsureDatabaseAsync();
+        await EnsureMigratedAsync();
     }
 
-    private Task EnsureDatabaseAsync() => _context.Database.MigrateAsync();
+    private Task EnsureMigratedAsync() => _context.Database.MigrateAsync();
+
+    private async Task<bool> CheckConnectionAsync()
+    {
+        try
+        {
+            DbContextOptionsBuilder<TContext> optionsBuilder = new();
+            optionsBuilder.UseNpgsql(ConnectionString);
+            
+            _context = (TContext)Activator.CreateInstance(
+                typeof(TContext),
+                optionsBuilder.Options);
+            if (_context == null) return false;
+
+            await _context?.Database.ExecuteSqlRawAsync("SELECT 1;");
+
+            return true;
+        }
+        catch
+        {
+            return false; 
+        }
+    }
 
     #region IDisposable
-    
-    private void ReleaseUnmanagedResources() => _context.Dispose();
+
+    private void ReleaseUnmanagedResources()
+    {
+        _context.Dispose();
+        _container.Dispose();
+    }
 
     public void Dispose()
     {
